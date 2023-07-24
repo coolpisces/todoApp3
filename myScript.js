df14aae0eeb6dc01bc6b863e6dca9e11279816dc
coolpisces <coolpisces22@gmail.com>
2023-07-25 01:18:54 +0300

    finished

--- myScript.js
+++ myScript.js
@@ -2,16 +2,19 @@ const add = document.querySelector('.add');
 const ul = document.querySelector('.todos');
 const search = document.querySelector('.search input');
 const button = document.querySelector('button');
+const inputs = document.querySelectorAll('.list-input');
 
 let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
 localStorage.setItem('items', JSON.stringify(itemsArray));
-const data = JSON.parse(localStorage.getItem('items'));
+let data = JSON.parse(localStorage.getItem('items'));
+
 
 
 const generateTemplate = todo => {
     const li = document.createElement('li');
     li.setAttribute("class", "list-group-item text-light d-flex justify-content-between align-items-center");
-    li.innerHTML = `<span>${todo}</span>`;
+    li.innerHTML = `<input class="list-input" type="text" readonly value= "${todo}"/>`;
+    li.innerHTML += `<button class="hide">Update</button>`;
     ul.appendChild(li);
     const newIcon = document.createElement('i');
     newIcon.setAttribute('class', 'far bi-trash3 delete');
@@ -28,17 +31,56 @@ add.addEventListener('submit', e => {
     if (todo.length != 0) {
         itemsArray.push(todo);
         localStorage.setItem('items', JSON.stringify(itemsArray));
+        data = JSON.parse(localStorage.getItem('items'));
         generateTemplate(todo);
         add.reset();
     }
 
 });
 
+function update() {
+    localStorage.setItem('items', JSON.stringify(data));
+    itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+    data = JSON.parse(localStorage.getItem('items'));
+}
+
 ul.addEventListener('click', e => {
     if (e.target.classList.contains('delete')) {
         e.target.parentElement.remove();
+        //console.log(e.target.parentElement.firstChild.textContent);
+        //console.log(data)
+        for (let i = 0; i < data.length; i++)
+            if (data[i] === e.target.parentElement.firstChild.value) {
+                console.log(data[i])
+                data.splice(i, 1);
+                update();
+            }
     }
 })
+
+ul.addEventListener('click', e => {
+    for (let i = 0; i < data.length; i++) {
+        if (data[i] === e.target.value) {
+            e.target.removeAttribute('readonly');
+        }
+    }
+})
+
+inputs.forEach((input) => {
+    input.addEventListener('change', e => {
+        console.log('olacak')
+        for (let i = 0; i < data.length; i++) {
+            if (data[i] === e.target.value) {
+                e.target.setAttribute('readonly');
+                data[i] = e.target.value;
+                console.log(data[i])
+                update();
+            }
+        }
+    })
+});
+
 
 button.addEventListener('click', function () {
     localStorage.clear();
